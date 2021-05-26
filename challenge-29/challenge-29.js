a647c3eb049915c85e5e7cf51710664b793baf69
HildebrandoLima <47666194+HildebrandoLima@users.noreply.github.com>
2021-05-26 15:09:58 -0300

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -35,5 +35,15 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+  
+  function app() {
+    return {
+      init: function() {
+        
+      }
+    };
+  }
+  
+  app.().init();
 
 })();

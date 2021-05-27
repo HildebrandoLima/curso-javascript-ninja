775eb08c402a6555c10067e843edb1d6c3f0d58e
HildebrandoLima <47666194+HildebrandoLima@users.noreply.github.com>
2021-05-27 15:01:15 -0300

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function($) {
   'use strict';
 
   /*
@@ -36,10 +36,45 @@
   que será nomeado de "app".
   */
   
-  function app() {
+  var app = (function app() {
     return {
       init: function() {
         this.companyInfo();
+        this.initEvents();
+      },
+      
+      initEvents: function initEvents() {
+        $('[data-js="form-register"]').on('submit', this.handleSubmit)
+      },
+      
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefaul();
+        var $tableCar.appendChild(app.createNewCar());
+      },
+      
+      createNewCar: function createNewCar() {
+        var $fragment = document.createDocumentFragment();
+        var $tr = document.createElement('tr');
+        var $tdImage = codument.createElement('td');
+        var $tdBrand = codument.createElement('td');
+        var $tdYear = codument.createElement('td');
+        var $tdPlate = codument.createElement('td');
+        var $tdColor = codument.createElement('td');
+        
+        $image.setAttribute('src',  $('[data-js="image"]').get().value);
+        $tdImage.appendChild($image);
+        $tdBrand.textContent = $('[data-js="model"]').get().value;
+        $tdYear.textContent = $('[data-js="year"]').get().value;
+        $tdPlate.textContent = $('[data-js="plate"]').get().value;
+        $tdColor.textContent = $('[data-js="color"]').get().value;
+        
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
+        
+        return $fragment.appendChild($tr);
       },
       
       companyInfo: funtion companyInfo() {
@@ -50,21 +85,21 @@
       },
       
       getCompanyInfo: function getCompanyInfo() {
-        if(!app().isReady().call(this))
+        if(!app.isReady().call(this))
           return;
           
           var data = JSON.parse(this.responseText);
-          var $companyName = new DOM('[data-js="company-name"]');
-          var $companyPhone = new DOM('[data-js="company-phone"]');
-          $companyName.get()[0].textContent = data.name;
-          $companyPhone.get()[0].textContent = data.phone;
+          var $companyName = $('[data-js="company-name"]').get();
+          var $companyPhone = $('[data-js="company-phone"]').get();
+          $companyName.textContent = data.name;
+          $companyPhone.textContent = data.phone;
       },
 
       isReady: function isReady() {
         return this.readyState === 4 && this.status === 200;
       };
-  }
+  })();
   
-  app.().init();
+  app.init();
 
 })();
